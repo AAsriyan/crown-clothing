c3513192ce92f597a7959325b43edc0f3db35c92
AAsriyan <aasriyan@protonmail.com>
2019-07-20 21:06:53 -0400

    Added homepage component in main App file

--- cc-app/src/App.js
+++ cc-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './homepage';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<h1>App</h1>
+			<HomePage />
 		</div>
 	);
 }
