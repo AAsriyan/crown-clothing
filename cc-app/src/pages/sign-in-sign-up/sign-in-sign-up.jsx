b2fa2b57d3a6914ca58de237413b0ff8dfee8565
AAsriyan <aasriyan@protonmail.com>
2019-07-21 22:51:14 -0400

    Added signin/signup component

---
+++ cc-app/src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.jsx';
+
+import './sign-in-sign-up.scss';
+
+const SignInSignUp = () => {
+	return (
+		<div className="sign-in-and-sign-up">
+			<SignIn />
+		</div>
+	);
+};
+
+export default SignInSignUp;
