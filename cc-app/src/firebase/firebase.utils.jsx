0f857330e376cde669c0abb5c3e49c52e80e2d38
AAsriyan <aasriyan@protonmail.com>
2019-07-22 00:32:47 -0400

    Added createUserProfile function

--- cc-app/src/firebase/firebase.utils.jsx
+++ cc-app/src/firebase/firebase.utils.jsx
@@ -12,6 +12,32 @@ const config = {
 	appId: '1:505180710730:web:e0628ae7b6ac183d'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (error) {
+			console.log('error creating user', error.message);
+		}
+	}
+
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
