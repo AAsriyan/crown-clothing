3a733c4e6b9763f45b0b54ef6128d8252991c281
AAsriyan <aasriyan@protonmail.com>
2019-07-21 22:50:07 -0400

    Changed to class and added firebase Auth

--- cc-app/src/App.js
+++ cc-app/src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.jsx';
+import ShopPage from './pages/shop/shop.jsx';
+import Header from './components/header/header.jsx';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.jsx';
+import { auth } from './firebase/firebase.utils.jsx';
+
 import './App.css';
 
-const HatsPage = () => (
-	<div>
-		<h1>HATS PAGE</h1>
-	</div>
-);
+class App extends Component {
+	state = {
+		currentUser: null
+	};
 
-function App() {
-	return (
-		<div className="App">
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route path="/hats" component={HatsPage} />
-			</Switch>
-		</div>
-	);
+	unsubscribeFromAuth = null
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({ currentUser: user });
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Header currentUser={this.state.currentUser}/>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route path="/shop" component={ShopPage} />
+					<Route path="/signin" component={SignInSignUp} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
