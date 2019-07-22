65e1b3bb92dc2399d57741eb05d02848e10db229
AAsriyan <aasriyan@protonmail.com>
2019-07-22 00:33:07 -0400

    imported sign up into file

--- cc-app/src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ cc-app/src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.jsx';
+import SignUp from '../../components/sign-up/sign-up.jsx';
 
 import './sign-in-sign-up.scss';
 
@@ -7,6 +8,7 @@ const SignInSignUp = () => {
 	return (
 		<div className="sign-in-and-sign-up">
 			<SignIn />
+			<SignUp />
 		</div>
 	);
 };
