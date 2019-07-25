9e02ff4e226f37e5caeddbb2795a524e2f2e5651
AAsriyan <aasriyan@protonmail.com>
2019-07-25 00:37:17 -0400

    Added stripe integration into checkout

--- cc-app/src/pages/checkout/checkout.jsx
+++ cc-app/src/pages/checkout/checkout.jsx
@@ -8,6 +8,7 @@ import {
 } from '../../redux/cart/cart.selectors.js';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.jsx';
+import StripCheckoutButton from '../../components/stripe-button/stripe-button.jsx';
 
 import './checkout.scss';
 
@@ -37,6 +38,12 @@ const CheckoutPage = ({ cartItems, total }) => {
 			<div className="total">
 				<span>{`TOTAL: $${total}`}</span>
 			</div>
+			<div className="test-warning">
+				*Please use the following test credit card for payments*
+				<br />
+				4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+			</div>
+			<StripCheckoutButton price={total} />
 		</div>
 	);
 };
