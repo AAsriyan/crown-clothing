9a7ff0d09136b5cb3b4b7ea78f1797bde31d48cd
AAsriyan <aasriyan@protonmail.com>
2019-08-15 10:12:40 -0400

    moved to sagas from app

--- cc-app/src/App.js
+++ cc-app/src/App.js
@@ -2,37 +2,23 @@ import React, { Component } from 'react';
 import { createStructuredSelector } from 'reselect';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions.js';
+
 import HomePage from './pages/homepage/homepage.jsx';
 import ShopPage from './pages/shop/shop.jsx';
 import Header from './components/header/header.jsx';
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.jsx';
 import CheckoutPage from './pages/checkout/checkout.jsx';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils.jsx';
-import { selectCurrentUser } from './redux/user/user.selectors.js';
 
+import { selectCurrentUser } from './redux/user/user.selectors.js';
+import { checkUserSession } from './redux/user/user.actions.js';
 import './App.css';
 
 class App extends Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		const { setCurrentUser } = this.props;
-
-		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-			if (userAuth) {
-				const userRef = await createUserProfileDocument(userAuth);
-
-				userRef.onSnapshot(snapShot => {
-					setCurrentUser({
-						id: snapShot.id,
-						...snapShot.data()
-					});
-				});
-			}
-
-			setCurrentUser(userAuth);
-		});
+		const { checkUserSession } = this.props;
+		checkUserSession();
 	}
 
 	componentWillUnmount() {
@@ -65,7 +51,7 @@ const mapStateToProps = createStructuredSelector({
 });
 
 const mapDispatchToProps = dispatch => ({
-	setCurrentUser: user => dispatch(setCurrentUser(user))
+	checkUserSession: () => dispatch(checkUserSession())
 });
 
 export default connect(
